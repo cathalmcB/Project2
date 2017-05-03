8938eb59ec02441c637a315ce92014cb3690d4a3
cathalmcB <cathalmc1@hotmail.co.uk>
2017-05-03 20:10:19 +0100

    The Sketches for Wake up Alarm System

---
+++ clientsketch/clientsketch.ino
@@ -0,0 +1,54 @@
+#include <Bridge.h>
+#include <BridgeClient.h>
+#include <BridgeServer.h>
+#include <BridgeSSLClient.h>
+#include <BridgeUdp.h>
+#include <Console.h>
+#include <FileIO.h>
+#include <HttpClient.h>
+#include <Mailbox.h>
+#include <Process.h>
+
+byte server [] = { 192, 168, 2, 7 }; //This is the IP address of the arduino we want to send data to.
+const int buttonPin = 7;  //There is a button connected to this arduino. When pressed it will send a ssignal to the other arduino.
+BridgeClient client;  // Initiate the arduino as a client named "client".
+int buttonState = 0;
+
+void setup() {
+ Serial.begin(9600);
+ 
+ delay(1000);
+ 
+ pinMode(buttonPin,INPUT);  //Set button as an input.
+ Bridge.begin();
+ if (client.connect(server, 5555)) //Attempt to connect to server arduino, on port 5555 (designated arduino port)
+ {
+    Serial.println("connected");
+ } 
+ else 
+ {
+   Serial.println("connection failed");
+ }
+}
+
+void loop() {
+
+   if(client.available())  
+   {
+    Serial.println("Still Connected");
+    buttonState = digitalRead(buttonPin); //Once connected read button state
+    if(buttonState == 1)
+    {
+      client.write(1);  //Send a 1 to other arduino
+    }
+    else
+    {
+      client.write(0);  //Send a 0 across
+    }
+   }
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+}
---
+++ serverSketch/serverSketch.ino
@@ -0,0 +1,74 @@
+#include <Bridge.h>
+#include <BridgeClient.h>
+#include <BridgeServer.h>
+#include <BridgeSSLClient.h>
+#include <BridgeUdp.h>
+#include <Console.h>
+#include <FileIO.h>
+#include <HttpClient.h>
+#include <Mailbox.h>
+#include <Process.h>
+
+byte serverIP[] = { 192, 168, 2, 7 };
+const int buzzerPin = 7;  //This arduino has an alarm connected to it. So it received a signal and the alarm goes off.
+const int ledPin = 3;     //Light will also go on.
+
+// The one global server object listening for incoming connections.
+BridgeServer server(serverIP, 5555);
+BridgeClient client;
+
+
+void setup()
+{
+   Bridge.begin();
+   Serial.begin(19200);
+   while (!Serial)
+      ;  // Wait until a Serial Monitor is connected.
+    
+   Server.noListenOnLocalhost(); // Listen for external connections
+   Server.begin();
+
+   pinMode(buzzerPin,OUTPUT);   //Initiate two outputs, LED and Buzzer.
+   pinMode(ledPin,OUTPUT);
+}
+
+
+void loop()
+{
+      digitalWrite(buzzerPin,LOW);  //Set two outputs to off at beginning
+      digitalWrite(ledPin,LOW);
+      
+      if (client.connected()) // Is it connected?
+      {
+        process();          // It's connected, process the client
+      }
+      else
+      {
+        Serial.println("Nothing Connected");
+      }
+}
+
+
+
+void process()
+{
+   // Read and echo all available data from the client.
+   int received;
+
+   // Read all available data, append to string
+   while (client.available())
+   {
+      received=client.read();
+   }
+      
+   // Got something? If so, print out what was recevied, send client ID back to other side
+   if (received != null)
+   {
+      digitalWrite(ledPin,HIGH);  //IF something came through turn LED ON
+
+      if(received == 1)
+      {
+        digitalWrite(buzzerPin,HIGH); //IF a one came through (ie-Button was pressed) turn on buzzer.
+      }
+   }
+}
